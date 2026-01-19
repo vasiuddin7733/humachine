92c0113e81b112cd06b66daa7337bd8056410fee
vasiuddin <vasiuddin.mohammed1@gmail.com>
2026-01-19 20:22:34 +0530

    set up tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0F172A",     // dark navy
+        secondary: "#2563EB",   // blue
+        accent: "#22C55E"       // green
+      }
+    }
+  },
+  plugins: [],
+}
